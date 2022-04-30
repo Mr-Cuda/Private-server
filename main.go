1baa080b6baedd97960e2477d01ede1e10a23cb5
Mr-Cuda <104436099+Mr-Cuda@users.noreply.github.com>
2022-04-30 11:55:03 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,13 +2,6 @@ package main
 
 import (
 	"fmt"
-
-	""
-	""
-
-	""
-	""
-	""
 )
 
 func main() {
@@ -25,6 +18,6 @@ func main() {
 	routes.SetupRoutes(app)
 
 	// Start and run the server
-	app.Run(":5000")
+	app.Run(":4000")
 
 }
