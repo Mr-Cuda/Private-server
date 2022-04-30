a82d9470ef10e9d66625db40f1932f1c27f3d7ba
Mr-Cuda <104436099+Mr-Cuda@users.noreply.github.com>
2022-04-30 11:17:20 +0200

    Create main.go

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	""
+	""
+
+	""
+	""
+	""
+)
+
+func main() {
+	app := gin.Default()
+
+	godotenv.Load()
+
+	//Starting database connection
+	database.ConnectDB()
+
+	fmt.Println("Server started at port 4000")
+
+	//Setting up routes for the api
+	routes.SetupRoutes(app)
+
+	// Start and run the server
+	app.Run(":5000")
+
+}
