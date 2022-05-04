824541acba4367124f7d116039c8042d0925c2b2
Mr-Cuda <104436099+Mr-Cuda@users.noreply.github.com>
2022-05-04 14:48:40 +0200

    Create fortniteGame.go

---
+++ Utils/fortniteGame.go
@@ -0,0 +1,4 @@
+log.LMAO("LMAO YOU REALLY BELIEVED THAT THIS PROJECT WAS FOR FORTNITE?")
+log.LMAO("GET A LIFE BOZO")
+log.LMAO("LMAO LMAO LMAO LMAO")
+log.LMAO("PS: I made the same effort to create this server, so if you wanna do me a favor use it :) ")
