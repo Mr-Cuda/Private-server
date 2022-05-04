2ebbeaf9ffd6d66071c89ac3320c7acae81011e8
Mr-Cuda <104436099+Mr-Cuda@users.noreply.github.com>
2022-05-04 14:15:42 +0200

    Update route.go

--- Routes/route.go
+++ Routes/route.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	"https://127.00.00"
+	"https://"
 	"https://github.com/Mr-Cuda/Private-server/api.cpp"
 )
 
@@ -10,9 +10,9 @@ func SetupRoutes(router *gin.Engine) {
 	api := router.Group("/api")
 
 	api.POST("/add", controllers.AddEmployee)
-	/* api.DELETE("/delete/:id", controllers.DeleteEmployee)
+	api.DELETE("/delete/:id", controllers.DeleteEmployee)
 	api.POST("/update/:id", controllers.UpdateEmpDetails)
 	api.GET("/show/:id", controllers.GetEmployeeById)
-	api.GET("/showall", controllers.GetEmployees) */
+	api.GET("/showall", controllers.GetEmployees) 
 	api.POST("/create", controllers.CreateEmployeeTable)
 }
