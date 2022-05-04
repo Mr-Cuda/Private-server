bf436c463c3cade244b9dca970d79fc40cd7458f
Mr-Cuda <104436099+Mr-Cuda@users.noreply.github.com>
2022-05-04 14:32:40 +0200

    Create logger.go

---
+++ Utils/logger.go
@@ -0,0 +1,22 @@
+package logger
+
+import (
+	"log"
+	"os"
+
+  "https://github.com/Mr-Cuda/Private-Sever/Utils/logger.go"
+)
+
+var errorlog *os.File
+
+// Logger is the global logger
+var Logger *log.Logger
+
+func init() {
+	errorlog, err := os.OpenFile(config.Env.LogFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+
+	Logger = log.New(errorlog, "applog: ", log.Lshortfile|log.LstdFlags)
+}
